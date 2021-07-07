602d54916ed7ef0b49311282695c14e1a5c2457f
sazzad4677 <sazzad35-1898@diu.edu.bd>
2021-07-07 10:54:19 +0600

    get all,put one, delete

--- routeHandler/todo-handler.js
+++ routeHandler/todo-handler.js
@@ -5,10 +5,43 @@ const todoSchema = require("../schemas/todoSchema");
 const Todo = new mongoose.model("Todo", todoSchema);
 
 //Get all the todos
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  await Todo.find({ status: "active" })
+    .select({
+      _id: 0,
+      __v: 0,
+      date: 0,
+    })
+    .limit(2)
+    .exec((err, data) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was an server side error",
+        });
+      } else {
+        res.status(200).json({
+          result: data,
+          message: "Todo was found successfully",
+        });
+      }
+    });
+});
 
 //get a todo by id
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  await Todo.find({ _id: req.params.id }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was an server side error",
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "Todo was found successfully",
+      });
+    }
+  });
+});
 
 //post a todo
 router.post("/", (req, res) => {
@@ -16,7 +49,7 @@ router.post("/", (req, res) => {
   newTodo.save((err) => {
     if (err) {
       res.status(500).json({
-        error: "There was an server side error"
+        error: "There was an server side error",
       });
     } else {
       res.status(200).json({
@@ -28,42 +61,81 @@ router.post("/", (req, res) => {
 
 //post multiple todo
 router.post("/all", async (req, res) => {
-   await Todo.insertMany(req.body, (err) => {
-        if(err) {
-            res.status(500).json({
-                error: "There was an server side error"
-              });
-        }
-        else{
-            res.status(200).json({
-                message: "Todos were inserted successfully",
-              });
-        }
-    })
+  await Todo.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was an server side error",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todos were inserted successfully",
+      });
+    }
+  });
 });
 
 //put a todo
 router.put("/:id", async (req, res) => {
-    await Todo.updateOne({_id: req.params.id}, {
-        $set: {
-            status: 'active'
-        }
-    }, (err) => {
-        if(err) {
-            res.status(500).json({
-                error: "There was an server side error"
-              });
-        }
-        else{
-            res.status(200).json({
-                message: "Todo update successfully",
-              });
-        }
+  await Todo.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "active",
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was an server side error",
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo update successfully",
+        });
+      }
     }
-    )
+  );
+});
+//put a todo
+router.put("/:id", async (req, res) => {
+  await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "active",
+      },
+    },
+    {
+      new: True,
+      useFindAndModify: false,
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was an server side error",
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo update successfully",
+        });
+      }
+    }
+  );
 });
 
 //delete todo
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was an server side error",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo was deleted successfully",
+      });
+    }
+  });
+});
 
 module.exports = router;
